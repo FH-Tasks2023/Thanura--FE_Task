010e72d3219b4d3a376e19f79d2c31a9ab80ec9b
ThanuraSilva <thanurasilva1989@gmail.com>
2023-02-05 12:59:42 +0530

    Sketch home -2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { MiddleBarComponent } from './middle-bar/middle-bar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     ContactComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    MiddleBarComponent
   ],
   imports: [
     BrowserModule,
